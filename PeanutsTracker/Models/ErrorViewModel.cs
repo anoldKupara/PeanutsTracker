5427b350b97e496abb7beec60217a06d2afd07b8
Anold Kupara <kuparaanold20@gmail.com>
2023-01-24 10:21:41 +0200

    Add project files.

---
+++ PeanutsTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PeanutsTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
